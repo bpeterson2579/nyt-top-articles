052d48886f38fe681d91b48d9931c195311b7530
Brian Peterson <bpeterson1579@gmail.com>
2022-04-24 17:11:33 -0600

    Remove unnecessary files and add basic App component structure

---
+++ src/Components/App.js
@@ -0,0 +1,27 @@
+import '../CSS/App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      error: ''
+    }
+  }
+
+  componentDidMount = () => {
+    return fetch('https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=qjaCX4knVimJGIdJG6yYQCNckp0u0BgG')
+      .then(res => res.json())
+      .then(data=> this.setState({articles: data.results}))
+      .catch(err => this.setState({error: err}))
+  }
+
+  render() {
+    return (
+      <h1 className='title'>New York Times Top Stories</h1>
+      );
+  }
+}
+
+export default App;
